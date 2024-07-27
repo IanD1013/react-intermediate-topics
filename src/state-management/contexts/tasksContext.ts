11f83110d255e988bef0991d3e45e014a2ff778f
IanD1013 <ian.tung1013@gmail.com>
2024-07-28 11:05:31 +1200

    Sharing state using React Context

---
+++ src/state-management/contexts/tasksContext.ts
@@ -0,0 +1,15 @@
+import { Dispatch } from "react";
+import { Task, TaskAction } from "../reducers/tasksReducer";
+import React from "react";
+
+// this is the object that we are gonna transport using React Context
+interface TasksContextType {
+  tasks: Task[];
+  dispatch: Dispatch<TaskAction>;
+}
+
+const TasksContext = React.createContext<TasksContextType>(
+  {} as TasksContextType
+); // this is the default value of the context
+
+export default TasksContext;
