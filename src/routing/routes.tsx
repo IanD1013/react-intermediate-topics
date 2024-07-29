b383f2cde06696d347091dfbc222fb02f1ea4fef
IanD1013 <ian.tung1013@gmail.com>
2024-07-29 20:18:06 +1200

    Layout routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,6 +5,7 @@ import UserDetail from "./UserDetail";
 import UsersPage from "./UsersPage";
 import ErrorPage from "./ErrorPage";
 import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         children: [{ path: ":id", element: <UserDetail /> }],
       },
     ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [],
   },
 ]);
 
