df05bf64ac672b17c0fe8b0a35b64091b753c462
IanD1013 <ian.tung1013@gmail.com>
2024-07-29 19:16:56 +1200

    Get data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,10 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams(); // can extract parameter values from the url
+  const [searchParams, setSearchParams] = useSearchParams(); // can access and update query string parameters
+  const location = useLocation(); // can access the current location object
+
   return <p>User</p>;
 };
 
