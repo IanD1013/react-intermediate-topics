74592c7aa2c9ac81f80ec077c2c7789e3ed0ba00
IanD1013 <ian.tung1013@gmail.com>
2024-07-29 19:49:44 +1200

    Exercise - work with nested routes

--- src/routing/UserDetail.tsx
+++ src/routing/UserDetail.tsx
@@ -1,11 +1,11 @@
 import { useLocation, useParams, useSearchParams } from "react-router-dom";
 
-const UserDetailPage = () => {
+const UserDetail = () => {
   const params = useParams(); // can extract parameter values from the url
   const [searchParams, setSearchParams] = useSearchParams(); // can access and update query string parameters
   const location = useLocation(); // can access the current location object
 
-  return <p>User</p>;
+  return <p>User {params.id}</p>;
 };
 
-export default UserDetailPage;
+export default UserDetail;
